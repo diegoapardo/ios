e20ddcef117fea31d9b165a1a3f8b909c967240b
diegoapardo <me@diegopardo.com>
2018-05-02 08:44:57 -0600

    a

--- web.rb
+++ web.rb
@@ -12,16 +12,16 @@ require 'active_record'
 # )
 
 
-class User < ActiveRecord::Base
-end
+# class User < ActiveRecord::Base
+# end
 
 # ActiveRecord::Migration.create_table :users do |t|
 #   t.string :name
 #   t.string :password
 # end
 
-class App < Sinatra::Application
-end
+# class App < Sinatra::Application
+# end
 
 get '/' do
   'Hi ' + User.first.name + ', your password is ' + User.first.password

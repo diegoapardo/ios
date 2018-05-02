552bcdd603cf5e256bddf177a1b9146cd0a4b3f0
Diego Pardo <dpardo@inboundcs.com>
2018-05-02 09:35:27 -0600

    a

--- web.rb
+++ web.rb
@@ -3,25 +3,25 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'active_record'
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter  => "mysql2",
-#   :host     => "us-cdbr-iron-east-05.cleardb.net",
-#   :username => "baadd3ec66bd37",
-#   :password => "06105946",
-#   :database => "heroku_d621c641b841663"
-# )
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql2",
+  :host     => "us-cdbr-iron-east-05.cleardb.net",
+  :username => "baadd3ec66bd37",
+  :password => "06105946",
+  :database => "heroku_d621c641b841663"
+)
 
 
-# class User < ActiveRecord::Base
-# end
+class User < ActiveRecord::Base
+end
 
 # ActiveRecord::Migration.create_table :users do |t|
 #   t.string :name
 #   t.string :password
 # end
 
-# class App < Sinatra::Application
-# end
+class App < Sinatra::Application
+end
 
 get '/' do
   'Hi ' + User.first.name + ', your password is ' + User.first.password

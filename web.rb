1cd81c6abe470eb39a4194c4d878c3c733effc4b
diegoapardo <me@diegopardo.com>
2018-05-02 08:22:30 -0600

    1

--- web.rb
+++ web.rb
@@ -2,27 +2,28 @@ require 'rubygems'
 require 'sinatra'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => "mysql2",
-  :host     => "us-cdbr-iron-east-05.cleardb.net",
-  :username => "baadd3ec66bd37",
-  :password => "06105946",
-  :database => "heroku_d621c641b841663"
-)
-
-class User < ActiveRecord::Base
-end
-
-# ActiveRecord::Migration.create_table :users do |t|
-#   t.string :name
-#   t.string :password
+# ActiveRecord::Base.establish_connection(
+#   :adapter  => "mysql2",
+#   :host     => "us-cdbr-iron-east-05.cleardb.net",
+#   :username => "baadd3ec66bd37",
+#   :password => "06105946",
+#   :database => "heroku_d621c641b841663"
+# )
+#
+# class User < ActiveRecord::Base
+# end
+#
+# # ActiveRecord::Migration.create_table :users do |t|
+# #   t.string :name
+# #   t.string :password
+# # end
+#
+# class App < Sinatra::Application
 # end
 
-class App < Sinatra::Application
-end
-
 get '/' do
-  'Hi ' + User.first.name + ', your password is ' + User.first.password
+  # 'Hi ' + User.first.name + ', your password is ' + User.first.password
+  "hi"
 end
 
 get '/test/:name' do

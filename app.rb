b46bcb1eef653b8d61abec95ef80884a74baf0b6
diegoapardo <me@diegopardo.com>
2018-05-01 22:08:58 -0600

    commit

--- app.rb
+++ app.rb
@@ -4,19 +4,19 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
-  :host     => "localhost",
-  :username => "root",
-  :password => "",
-  :database => "ios"
+  :host     => "us-cdbr-iron-east-05.cleardb.net",
+  :username => "baadd3ec66bd37",
+  :password => "06105946",
+  :database => "heroku_d621c641b841663"
 )
 
 class User < ActiveRecord::Base
 end
 
-# ActiveRecord::Migration.create_table :users do |t|
-#   t.string :name
-#   t.string :password
-# end
+ActiveRecord::Migration.create_table :users do |t|
+  t.string :name
+  t.string :password
+end
 
 class App < Sinatra::Application
 end

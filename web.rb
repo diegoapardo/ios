7b5046a48069573cb022bb52f1de9ae630673451
diegoapardo <me@diegopardo.com>
2018-05-02 08:23:38 -0600

    a

--- web.rb
+++ web.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'active_record'
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter  => "mysql2",
-#   :host     => "us-cdbr-iron-east-05.cleardb.net",
-#   :username => "baadd3ec66bd37",
-#   :password => "06105946",
-#   :database => "heroku_d621c641b841663"
-# )
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql2",
+  :host     => "us-cdbr-iron-east-05.cleardb.net",
+  :username => "baadd3ec66bd37",
+  :password => "06105946",
+  :database => "heroku_d621c641b841663"
+)
 #
 # class User < ActiveRecord::Base
 # end

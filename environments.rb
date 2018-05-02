508f8facb2ec5a3d93564d08f539abdf56c43ff6
Diego Pardo <dpardo@inboundcs.com>
2018-05-02 09:50:34 -0600

    a

--- environments.rb
+++ environments.rb
@@ -1,3 +1,20 @@
+# #The environment variable DATABASE_URL should be in the following format:
+# # => postgres://{user}:{password}@{host}:{port}/path
+# configure :production do
+# 	db = URI.parse(ENV['DATABASE_URL'] )
+#
+# 	ActiveRecord::Base.establish_connection(
+# 			:adapter => 'mysql2'
+# 			:host     => db.host,
+# 			:username => db.user,
+# 			:password => db.password,
+# 			:database => db.path[1..-1],
+# 			:encoding => 'utf8'
+# 	)
+# end
+
+
+
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production do
@@ -5,10 +22,10 @@ configure :production do
 
 	ActiveRecord::Base.establish_connection(
 			:adapter => 'mysql2'
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
+			:host     => 'us-cdbr-iron-east-05.cleardb.net',
+			:username => 'baadd3ec66bd37',
+			:password => '06105946',
+			:database => 'heroku_d621c641b841663',
 			:encoding => 'utf8'
 	)
 end
